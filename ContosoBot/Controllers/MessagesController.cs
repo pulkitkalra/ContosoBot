638c45c9bd12db5a686977fce6684ad3dad14d89
Pulkit <Pulkit@PULKIT>
2016-11-21 19:21:45 +1300

    Add project files.

---
+++ ContosoBot/Controllers/MessagesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace ContosoBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity message)
+        {
+            if (message.Type == ActivityTypes.Message)
+            {
+                string StockRateString;
+                StockLUIS StLUIS = await GetEntityFromLUIS(message.Text);
+                if (StLUIS.intents.Count() > 0)
+                {
+                    switch (StLUIS.intents[0].intent)
+                    {
+                        case "StockPrice":
+                            StockRateString = await Stock.GetStock(StLUIS.entities[0].entity);
+                            break;
+                        // use below to get i
+                        /*
+                        case "StockPrice2":
+                            StockRateString = await Stock.GetStock(StLUIS.entities[0].entity);
+                            break;*/
+                        default:
+                            StockRateString = "Sorry, I am not getting you...";
+                            break;
+                    }
+                }
+                else
+                {
+                    StockRateString = "Sorry, I am not getting you...";
+                }
+
+                // return our reply to the user  
+                ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
+                // calculate something for us to return
+
+                // return our reply to the user
+                Activity reply = message.CreateReply(StockRateString);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(message);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+        private static async Task<StockLUIS> GetEntityFromLUIS(string Query)
+        {
+            Query = Uri.EscapeDataString(Query);
+            StockLUIS Data = new StockLUIS();
+            using (HttpClient client = new HttpClient())
+            {
+                string RequestURI = "https://api.projectoxford.ai/luis/v2.0/apps/4795ab7b-b2b9-413a-8e2b-92505c976e3f?subscription-key=f6ea8956f36b4379bcab23f342bfb460&q=" + Query
+                    + "&verbose=true";
+
+                HttpResponseMessage msg = await client.GetAsync(RequestURI);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var JsonDataResponse = await msg.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<StockLUIS>(JsonDataResponse);
+                }
+            }
+            return Data;
+        }
+    }
+
+    public class Stock
+    {
+        public static async Task<string> GetStock(string StockSymbol)
+        {
+            double? dblStockValue = await StockYahoo.GetStockRateAsync(StockSymbol);
+            if (dblStockValue == null)
+            {
+                return string.Format("This \"{0}\" is not an valid stock symbol", StockSymbol);
+            }
+            else
+            {
+                return string.Format("Stock Price of {0} is {1}", StockSymbol, dblStockValue);
+            }
+        }
+    }
+}
---
+++ ContosoBot/StockYahoo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+/**
+ * This class makes calls to the Yahoo API to get the required Stock.
+ * Reference: http://www.c-sharpcorner.com/article/an-interactive-bot-application-with-luis-using-microsoft-bot/
+ */
+namespace ContosoBot
+{
+    public class StockYahoo
+    {
+        public static async Task<double?> GetStockRateAsync(string StockSymbol)
+        {
+            try
+            {
+                string ServiceURL = $"http://finance.yahoo.com/d/quotes.csv?s={StockSymbol}&f=sl1d1nd";
+                string ResultInCSV;
+                using (WebClient client = new WebClient())
+                {
+                    ResultInCSV = await client.DownloadStringTaskAsync(ServiceURL).ConfigureAwait(false);
+                }
+                var FirstLine = ResultInCSV.Split('\n')[0];
+                var Price = FirstLine.Split(',')[1];
+                if (Price != null && Price.Length >= 0)
+                {
+                    double result;
+                    if (double.TryParse(Price, out result))
+                    {
+                        return result;
+                    }
+                }
+                return null;
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message); // get exception message.
+                throw ex;
+            }
+        }
+    }
+}
