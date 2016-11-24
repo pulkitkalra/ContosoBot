896a1016b13a18ad9d92d6c73df294ba9f465b66
Pulkit <pul.k3333@gmail.com>
2016-11-24 20:56:54 +1300

    set stocks as favourite and link up with Azure Easy Tables (Lost/ Stolen Cards)

---
+++ ContosoBot/DataModels/Timeline.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoBot.DataModels
+{
+    public class Timeline
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "cardNumber")]
+        public int CardNumber { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "lostTime")]
+        public String LostTime { get; set; }
+    }
+}
